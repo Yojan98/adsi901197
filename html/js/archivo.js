1f6778f3f91ca5fa1ce1c56defa013b488e85989
“Yojan <yojan65622@hotmail.com>
2015-12-10 14:43:07 -0500

    “Trabajos”

--- html/js/archivo.js
+++ html/js/archivo.js
@@ -1,3 +1,3 @@
 var mensaje = document.getElementById('mensaje');
-var nombre  = prompt("¿Cual es su nombre?");
-mensaje.innerHTML = " Bienvenido : " +nombre;
+var nombre = prompt("Cuál es su Nombre?");
+mensaje.innerHTML = "Bienvenido: "+nombre;
